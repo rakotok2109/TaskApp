836ec40182eef308dcce06cbca05c278fe5b8e29
arakot <nyarovantsoanathan.rakotonoera@eemi.com>
2025-02-20 14:23:30 +0100

    Initialization project with config files

---
+++ config/controller/PDOUtils.php
@@ -0,0 +1,52 @@
+<?php 
+
+class PDOUtils {
+    private $pdo_;
+    private static $sharedInstance_;
+
+    private function __construct() {
+        $this->pdo_ = new PDO('mysql:host=localhost;dbname=tasks_app', 'root', '', [  PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',]); }
+
+      
+        public static function getSharedInstance()
+        {
+            if (!isset(PDOUtils::$sharedInstance_)) {
+                PDOUtils::$sharedInstance_ = new PDOUtils();
+            }
+        
+            return PDOUtils::$sharedInstance_;
+        }
+
+        public function requestSQL($sql, $params = null) {
+            $statement = $this->pdo_->prepare($sql);
+            
+            if ($statement) {
+                if ($statement->execute($params)) {
+                   
+                    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+                    unset($statement);
+                    return $result;
+                } else {
+                    throw new Exception('Erreur lors de l\'exécution de la requête : ' . implode(", ", $statement->errorInfo()));
+                }
+            } else {
+                throw new Exception('Erreur lors de la préparation de la requête : ' . implode(", ", $this->pdo_->errorInfo()));
+            }
+        }
+        public function execSQL($sql, $params = null) {
+            $statement = $this->pdo_->prepare($sql);
+            if($statement && $statement->execute($params)) {
+               
+                unset($statement);
+                return true;
+            }
+            return false;
+           
+        }
+
+        public function lastInsertId() {
+            return $this->pdo_->lastInsertId();
+        }
+    
+      
+}
---
+++ config/init.php
@@ -0,0 +1,6 @@
+<?php 
+require_once __DIR__ .'/controller/PDOUtils.php';
+require_once __DIR__ .'/controller/TaskController.php';
+require_once __DIR__ .'/model/Task.php';
+require_once __DIR__ .'/conf.inc.php';
+?>
